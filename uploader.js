833f722986e85b4be05e8ad11d75cea0d4ab9126
partg952 <dankparth@gmail.com>
2024-12-28 21:04:31 +0530

    first commit

--- uploader.js
+++ uploader.js
@@ -1,7 +1,7 @@
 const { v2: cloudinary } = require("cloudinary");
 const fs = require("fs");
 const { createClient } = require("@supabase/supabase-js");
-const supabase = createClient("https://xydasqjisznapwzbyoiu.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5ZGFzcWppc3puYXB3emJ5b2l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI1MzIzNTksImV4cCI6MjA0ODEwODM1OX0.BrA7Rx19oAI0vErQdZ79ovkKeQZarBAL4YmPT74I3wY");
+const supabase = createClient("https://xydasqjisznapwzbyoiu.supabase.co", process.env.SUPABASE_KEY);
 
 
 class Uploader {
